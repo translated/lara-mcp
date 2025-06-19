d7a8adfa149b8c4d973796e82f2f8450e27073d5
Christopher Domenicano <ikiigaiiii@gmail.com>
2025-06-19 17:46:30 +0200

    chore: removed useless file

--- src/env.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-const envSchema = z.object({
-  ALLOWED_ORIGINS: z.string().transform((val) => val.split(",").map((origin) => origin.trim())),
-});
-
-export const env = envSchema.parse(process.env);
